4e66dac980e62bce75bb55ef803e3580e308d32f
Alexita <57197358+LaAlexita@users.noreply.github.com>
2024-10-07 15:53:51 +0200

    Update server.lua
    Hacemos que lo pueda usar todo el mundo, deshabilitamos los comandos y hacemos que sea el item usable.

--- integration/server.lua
+++ integration/server.lua
@@ -1,32 +1,23 @@
--- Server-side checks and integration of how the boombox UI is accessed.
--- Do whatever checks you want here and execute TriggerEvent('cs-boombox:toggleControllerInterface', source, uniqueId) with source being the player ID and uniqueId being the boombox unique ID to open the boombox UI in a player.
--- If your checks fail, execute TriggerEvent('cs-boombox:disallowControllerInterface', source) to block remove the controller access (e.g. in case of a dynamic check being updated).
--- The default integration triggers this via the client command boombox and does a perform native death check (client-side) and a permission check (server-side).
--- To allow all admins (using Ace group admin) to perform controller duties in all boombox models, add "add_ace group.admin cs-boombox.control allow" in your server's config file.
--- Alternatively to Ace permissions in the default integration, you can add any player identifiers to the array playerIdentifiersAsControllers below to allow specific players to perform controller duties in all boombox models.
--- You can keep the default integration and edit the default CanAccessControllerInterface function and return true / false based on your conditions.
--- Action boombox commands (/create-boombox, /destroy-boombox, /pickup-boombox, /drop-boombox) are also included in the default integration and are also checked against the same function. Feel free to remove them or edit them.
+ESX = exports["es_extended"]:getSharedObject()
 
-local playerIdentifiersAsControllers = {
-    'steam:000000000000000', -- Example Steam player identifier.
-    'fivem:000000', -- Example FiveM player identifier.
-}
+ESX.RegisterUsableItem('boombox', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    
+    TriggerClientEvent('cs-boombox:useBoombox', source)
+    
+    xPlayer.removeInventoryItem('boombox', 1)
+end)
+
+
 
 function CanAccessControllerInterface(source, model)
-    if (IsPlayerAceAllowed(source, 'cs-boombox.control')) then
-        return true
-    end
-
-    for i = 1, #playerIdentifiersAsControllers do
-        for ii, identifier in ipairs(GetPlayerIdentifiers(source)) do
-            if (string.lower(identifier) == string.lower(playerIdentifiersAsControllers[i])) then
-                return true
-            end
-        end
-    end
-
-    return false
+    return true  
 end
+
+RegisterNetEvent('cs-boombox:useBoomboxItem', function()
+    local source = source  
+    TriggerClientEvent('cs-boombox:create', source, 'prop_boombox_01')
+end)
 
 RegisterNetEvent('cs-boombox:integration:toggleControllerInterface', function(uniqueId, model)
     local source = source
@@ -40,37 +31,36 @@ end)
 
 -- Action Commands
 
-RegisterCommand('create-boombox', function(source, args, raw)
-    if (not CanAccessControllerInterface(source, 'prop_boombox_01')) then
-        return
-    end
+-- RegisterCommand('create-boombox', function(source, args, raw)
+--     if (not CanAccessControllerInterface(source, 'prop_boombox_01')) then
+--         return
+--     end
 
-    TriggerClientEvent('cs-boombox:create', source, 'prop_boombox_01')
-end)
+--     TriggerClientEvent('cs-boombox:create', source, 'prop_boombox_01')
+-- end)
 
-RegisterCommand('pickup-boombox', function(source, args, raw)
-    if (not CanAccessControllerInterface(source, 'prop_boombox_01')) then
-        return
-    end
+-- RegisterCommand('pickup-boombox', function(source, args, raw)
+--     if (not CanAccessControllerInterface(source, 'prop_boombox_01')) then
+--         return
+--     end
 
-    TriggerClientEvent('cs-boombox:pickup', source, 'prop_boombox_01')
-end)
+--     TriggerClientEvent('cs-boombox:pickup', source, 'prop_boombox_01')
+-- end)
 
-RegisterCommand('drop-boombox', function(source, args, raw)
-    if (not CanAccessControllerInterface(source, 'prop_boombox_01')) then
-        return
-    end
+-- RegisterCommand('drop-boombox', function(source, args, raw)
+--     if (not CanAccessControllerInterface(source, 'prop_boombox_01')) then
+--         return
+--     end
 
-    TriggerClientEvent('cs-boombox:drop', source, 'prop_boombox_01')
-end)
+--     TriggerClientEvent('cs-boombox:drop', source, 'prop_boombox_01')
+-- end)
 
-RegisterCommand('destroy-boombox', function(source, args, raw)
-    if (not CanAccessControllerInterface(source, 'prop_boombox_01')) then
-        return
-    end
+-- RegisterNetEvent('cs-boombox:destroyBoombox', function(model)
+--     local xPlayer = ESX.GetPlayerFromId(source)
+--     xPlayer.addInventoryItem('boombox', 1)  
+--     TriggerClientEvent('cs-boombox:destroy', source, model)  
+-- end)
 
-    TriggerClientEvent('cs-boombox:destroy', source, 'prop_boombox_01')
-end)
 
 -- Server Events
 
